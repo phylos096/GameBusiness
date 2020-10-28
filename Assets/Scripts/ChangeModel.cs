1cd8049ab430a3dfee2b895b844b6fba3fbbaf63
phylos096 <43868600+phylos096@users.noreply.github.com>
2020-10-28 15:43:33 +0900

    [add]Test用

---
+++ Assets/Scripts/ChangeModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeModel : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> SetModels = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void change()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
