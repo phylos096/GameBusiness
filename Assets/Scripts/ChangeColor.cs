8f0d3c3bf3182bd5d5b692771746c66733d739ce
phylos096 <43868600+phylos096@users.noreply.github.com>
2020-11-04 14:41:37 +0900

    [add]色の変更スクリプト

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    [SerializeField]
+    List<Material> setMaterials = new List<Material>();
+
+    Renderer rend;
+
+    //テスト用の変数
+    byte i;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //生成時に、起動
+        i = (byte)Random.Range(0,setMaterials.Count);
+
+        rend = GetComponent<Renderer>();
+        
+        changeColors(i);
+    }
+
+    /// <summary>
+    /// setMaterialsにセットされたマテリアルに変更をする　
+    /// </summary>
+    /// <param name="i"></param>
+    public void changeColors(byte i)
+    {
+        rend.material = setMaterials[i];
+    }
+}
