1d357875f30b37bfe125aa3798b729a6a4c8c721
phylos096 <43868600+phylos096@users.noreply.github.com>
2020-11-04 14:40:47 +0900

    [update]モデルの切り替えスクリプト

--- Assets/Scripts/ChangeModel.cs
+++ Assets/Scripts/ChangeModel.cs
@@ -1,29 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ChangeModel : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> SetModels = new List<GameObject>();
+    private List<GameObject> SetModels = new List<GameObject>();
+
+    //テスト用
+    byte i;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        i = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    public void change()
-    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            i = (byte)Random.Range(0, SetModels.Count);
+            changeShape(i);
 
+            //Destroy(this);
+            Destroy(this.gameObject);
         }
+    }
+
+    public void changeShape(byte i)
+    {
+        //GameObject newObj = new GameObject();
+
+        //生成する
+        /*newObj = GameObject.Instantiate(SetModels[i]);
+        newObj.transform.position = this.gameObject.transform.position;
+        newObj.transform.localScale = this.gameObject.transform.localScale;*/
+
+        //
+        Instantiate(SetModels[i], this.gameObject.transform.position,this.gameObject.transform.rotation);
+
     }
 }
